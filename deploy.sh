ff138abae7823c4558ecc0b018593a0d70289cf7
Michael Gajda <michael.gajda@tu-dortmund.de>
2018-10-28 01:41:44 +0200

    Update deploy-script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-(umask 077; echo "$SSH_KEY" > /tmp/id_deploy)
-rsync -e "ssh -i /tmp/id_deploy -oBatchMode=yes -p 51015" -r --delete public/ hacknsnack@iomona.de:hackandsnack.de
+
+# encode ssh_key with 'cat key | base64 -w 0'
+(umask 077; echo "$SSH_KEY_BASE64" | base64 -d > /tmp/id_deploy)
+
+rsync -e "ssh -i /tmp/id_deploy -o BatchMode=yes -o StrictHostKeyChecking=no -p 51015" -r --delete public/ hacknsnack@iomona.de:hackandsnack.de
 rm /tmp/id_deploy
