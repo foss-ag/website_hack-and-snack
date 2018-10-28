d9809bedd46bf130e7cba4d45ec339493835701d
Michael Gajda <michael.gajda@tu-dortmund.de>
2018-10-28 17:50:24 +0100

    Ignore attempts to save hostkey for deployment server

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # encode ssh_key with 'cat key | base64 -w 0'
 (umask 077; echo "$SSH_KEY_BASE64" | base64 -d > /tmp/id_deploy)
 
-rsync -e "ssh -i /tmp/id_deploy -o BatchMode=yes -o StrictHostKeyChecking=no -p 51015" -r --delete public/ hacknsnack@iomona.de:hackandsnack.de
+rsync -e "ssh -i /tmp/id_deploy -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 51015" -r --delete public/ hacknsnack@iomona.de:hackandsnack.de
 rm /tmp/id_deploy
